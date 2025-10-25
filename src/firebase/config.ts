81aab9df5f93160b68cb6ffde25722882168321a
Dhanush <22a41.dhanush@sjec.ac.in>
2025-10-25 09:35:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3213632928-6053e",
+  "appId": "1:469338230825:web:0eadd9df0ad6e4c95acc38",
+  "apiKey": "AIzaSyDqNbAdxCdGX_hHcWPc9KvciWVLM_kPBEM",
+  "authDomain": "studio-3213632928-6053e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "469338230825"
+};
