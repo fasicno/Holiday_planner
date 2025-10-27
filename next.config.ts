2242a5061934b3a9ce64edb819e97579959dfa63
Dhanush <22a41.dhanush@sjec.ac.in>
2025-10-27 14:33:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 
 const nextConfig: NextConfig = {
-  output: 'export',
   basePath: isGithubActions ? '/holiday-planner' : '',
   assetPrefix: isGithubActions ? '/holiday-planner/' : '',
   typescript: {
