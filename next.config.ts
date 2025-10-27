b56c8ea052074a95df8eabec0f74961adba6c54e
Dhanush <22a41.dhanush@sjec.ac.in>
2025-10-27 16:12:26 +0000

    correct it

--- next.config.ts
+++ next.config.ts
@@ -40,7 +40,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
